bbcf54647e34eb1b1ea6e5a586d99d36512cd238
Michael McLoughlin <mmcloughlin@gmail.com>
2016-12-09 18:27:26 -0800

    formatting of inserted snippets

--- handlers.go
+++ handlers.go
@@ -1,18 +1,27 @@
 package cite
 
+func FormatSnippet(lines []string) []string {
+	output := make([]string, len(lines)+2)
+	for i, line := range lines {
+		output[i+1] = "\t" + line
+	}
+	return output
+}
+
 func InsertHandler(r Resource, lines []string) ([]string, []string, error) {
 	ref := Directive{
 		ActionRaw: "Reference",
 		Citation:  r.Cite(),
 	}
-	insertion := []string{ref.String()}
+	insertion := []string{" " + ref.String()}
 
 	snippet, err := Fetch(r)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	insertion = append(insertion, snippet...)
+	formatted := FormatSnippet(snippet)
+	insertion = append(insertion, formatted...)
 
 	return insertion, lines[1:], nil
 }
