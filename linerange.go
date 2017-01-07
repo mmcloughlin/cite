ddded362e3227d7e29bf9ca845fbc5c2fda38b21
Michael McLoughlin <mmcloughlin@gmail.com>
2017-01-07 18:52:20 +0000

    doc on LineRange

--- linerange.go
+++ linerange.go
@@ -8,21 +8,28 @@ import (
 )
 
 var (
-	ErrBadLineRange       = errors.New("cite: bad line range")
+	// ErrBadLineRange is returned when bad upper and lower bounds are used to
+	// construct a LineRange.
+	ErrBadLineRange = errors.New("cite: bad line range")
+
+	// ErrBadLineRangeFormat is returned for a parsing error.
 	ErrBadLineRangeFormat = errors.New("cite: bad line range format")
 )
 
-// LineRange
+// LineRange represents a range of one or more lines.
 type LineRange struct {
 	start int
 	end   int
 }
 
+// NewSingleLine returns a LineRange object for a single line.
 func NewSingleLine(n int) LineRange {
 	l, _ := NewLineRange(n, n)
 	return l
 }
 
+// NewLineRange returns a LineRange object for the lines between s and e
+// (inclusive). It errors if the start and end do not make sense (e < s).
 func NewLineRange(s, e int) (LineRange, error) {
 	if e < s {
 		return LineRange{}, ErrBadLineRange
@@ -33,6 +40,9 @@ func NewLineRange(s, e int) (LineRange, error) {
 	}, nil
 }
 
+// ParseLineRange constructs a LineRange from a string representation. An
+// example for the typical case is "3-17" for the LineRange between 3 and 17.
+// A single digit is parsed into a LineRange for a single line.
 func ParseLineRange(s string) (LineRange, error) {
 	parts := strings.Split(s, "-")
 	if len(parts) > 2 {
@@ -63,13 +73,18 @@ func (l LineRange) String() string {
 	return fmt.Sprintf("%d-%d", l.start, l.end)
 }
 
+// Start returns the first line in the range (inclusive).
 func (l LineRange) Start() int { return l.start }
-func (l LineRange) End() int   { return l.end }
 
+// End returns the last line in the range (inclusive).
+func (l LineRange) End() int { return l.end }
+
+// LineIncluded returns whether line n is included in the range.
 func (l LineRange) LineIncluded(n int) bool {
 	return l.start <= n && n <= l.end
 }
 
+// NumLines returns the number of lines in the range.
 func (l LineRange) NumLines() int {
 	return l.end - l.start + 1
 }
